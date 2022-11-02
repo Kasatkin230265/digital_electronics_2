04faf48b679aa1c6044f8960922711274f7da301
Kasatkin230265 <xkasat00@vutbr.cz>
2022-11-02 12:48:43 +0100

    main

---
+++ labs/06-uart/06-uart/src/main.c
@@ -0,0 +1,110 @@
+/***********************************************************************
+ * 
+ * Use USART unit and transmit data between ATmega328P and computer.
+ * 
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ * Copyright (c) 2018 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+
+/* Defines -----------------------------------------------------------*/
+#ifndef F_CPU
+# define F_CPU 16000000  // CPU frequency in Hz required for UART_BAUD_SELECT
+#endif
+
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <uart.h>           // Peter Fleury's UART library
+#include <stdlib.h>         // C library. Needed for number conversions
+
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Use Timer/Counter1 and transmit UART data four times 
+ *           per second.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    // Initialize USART to asynchronous, 8N1, 9600
+    uart_init(UART_BAUD_SELECT(9600, F_CPU));
+            
+    // Configure 16-bit Timer/Counter1 to transmit UART data
+    // Set prescaler to 262 ms and enable overflow interrupt
+    TIM1_overflow_262ms();
+    TIM1_overflow_interrupt_enable();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Put strings to ringbuffer for transmitting via UART
+    uart_puts("\x1b[4;32m");
+    uart_puts("                                     \r\n");
+    uart_puts("\x1b[4;32m");
+    uart_puts("Char | Decimal | Hexadecimal | Binary\t\r\n");
+    uart_puts("\x1b[0m");   
+    
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Transmit UART data four times per second.
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{
+  uint8_t value;
+  char string[8];  // String for converted numbers by itoa()
+
+  value = uart_getc();
+  if (value != '\0') {
+
+    if (value == 27)
+    uart_puts("esc\t");
+    else if (value == 32)
+    uart_puts("space\t");
+    else if (value == 9)
+    uart_puts("tab\t");
+    else if (value == 127)
+    uart_puts("backspace\t");
+    else if (value == 13)
+    uart_puts("enter\t");
+    else {
+
+    uart_putc(value);
+    uart_puts("\t");
+
+    }
+
+    itoa(value, string, 10);
+    uart_puts(string);
+    uart_puts("\t 0x");
+
+    itoa(value, string, 16);
+    uart_puts(string);
+    uart_puts("\t       0b");
+
+    itoa(value, string, 2);
+    uart_puts(string);
+    uart_puts("\r\n");
+ 
+  }
+}
